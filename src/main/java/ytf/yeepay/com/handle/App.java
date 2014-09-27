1f89de6a60c089604d5471099e0ee44f376cba50
nvee <1059587098@qq.com>
2014-09-27 10:24:40 +0800

    2014-9-27 10:24:34

---
+++ src/main/java/ytf/yeepay/com/handle/App.java
@@ -0,0 +1,13 @@
+package ytf.yeepay.com.handle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
